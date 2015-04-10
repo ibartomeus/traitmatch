f97ef472a05050e33fd13f1cfebacc2ddd0f82f5
ibartomeus <ibartomeus@home>
2015-04-10 16:54:41 +0200

    Add new file with fitted models to the real datasets

---
+++ R/Fit_data.R
@@ -0,0 +1,150 @@
+#Run real data
+
+source('R/FitModel_SA.R')
+
+# Load data for Pred/Prey
+#I DIDN'T UPLOAD THE DATA TO GIT BECAUSE THE REPO WILL GO OPEN EVENTUALLY AND I DON'T KNOW IF THIS DATA IS PUBLIC
+original_data = read.csv2("temp/size_Barnes2008.csv",dec=".")
+head(original_data)
+MPred = log10(original_data$standardised_predator_length)
+MPrey = log10(original_data$si_prey_length)
+
+
+#pred/prey----
+pars_pre <- fit_it(integrated_model,
+                   Tlevel1 = MPred, 
+                   Tlevel2 = MPrey,
+                   mean_Tlevel1 = mean(MPred),
+                   sd_Tlevel1 = sd(MPrey))
+
+plot.pred(pars = pars_pre, Tlevel1 = MPred, 
+          Tlevel2 = MPrey)
+
+#model comparision
+pars_pre_niche <- fit_it(niche_model,
+                   Tlevel1 = MPred, 
+                   Tlevel2 = MPrey,
+                   mean_Tlevel1 = mean(MPred),
+                   sd_Tlevel1 = sd(MPrey))
+plot.pred(pars = pars_pre_niche, Tlevel1 = MPred, 
+          Tlevel2 = MPrey)
+#likelihoods
+lh_model <- -integrated_model(pars_pre, MPred, MPrey, mean(MPred),sd(MPrey))
+lh_niche <- -niche_model(pars_pre_niche, MPred, MPrey, mean(MPred), sd(MPrey))
+lh_neutral <- -neutral_model(MPred, MPrey, mean(MPred), sd(MPrey))
+
+barplot(c(lh_model, lh_niche, lh_neutral))
+
+# Load data for grasshoppers
+grass = read.table("data/Deraison2014.txt", h = TRUE)
+head(grass)
+grass <- subset(grass, Herbivory > 10)
+
+#grasshoppers--------
+pars_grass_bin <- fit_it(integrated_model,
+                         Tlevel1 = grass$G.Incisive.strength, 
+                         Tlevel2 = grass$P.Leaf.carbon.nitrogen.ratio,
+                         mean_Tlevel1 = mean(grass$G.Incisive.strength),
+                         sd_Tlevel1 = sd(grass$G.Incisive.strength))
+
+plot.pred(pars = pars_grass_bin, Tlevel1 = grass$G.Incisive.strength, 
+          Tlevel2 = grass$P.Leaf.carbon.nitrogen.ratio, xlab = "C:N ratio", ylab = "Incisive strength")
+#model comparision
+pars_grass_bin_niche = fit_it(niche_model, 
+                   Tlevel1 = grass$G.Incisive.strength, 
+                   Tlevel2 = grass$P.Leaf.carbon.nitrogen.ratio,
+                   mean_Tlevel1 = mean(grass$G.Incisive.strength),
+                   sd_Tlevel1 = sd(grass$G.Incisive.strength))
+plot.pred(pars = pars_grass_bin_niche, Tlevel1 = grass$G.Incisive.strength, 
+          Tlevel2 = grass$P.Leaf.carbon.nitrogen.ratio, xlab = "C:N ratio", ylab = "Incisive strength")
+#likelihoods
+lh_model <- -integrated_model(pars_grass_bin, grass$G.Incisive.strength, grass$P.Leaf.carbon.nitrogen.ratio, 
+                              mean(grass$G.Incisive.strength), sd(grass$G.Incisive.strength))
+lh_niche <- -niche_model(pars_grass_bin_niche, grass$G.Incisive.strength, grass$P.Leaf.carbon.nitrogen.ratio, 
+                         mean(grass$G.Incisive.strength), sd(grass$G.Incisive.strength))
+lh_neutral <- -neutral_model(grass$G.Incisive.strength, grass$P.Leaf.carbon.nitrogen.ratio, 
+                             mean(grass$G.Incisive.strength), sd(grass$G.Incisive.strength))
+
+barplot(c(lh_model, lh_niche, lh_neutral))
+
+# Make frequentistic grasshoper data
+grass = read.table("data/Deraison2014.txt", h = TRUE)
+grass <- subset(grass, Herbivory > 0)
+head(grass)
+
+#NOTE: If model is sentistive to sample size, which may not be, 
+#this may be an arbitrary way to convert to frequencies.
+Incisive.strength <- c()
+for(i in 1:nrow(grass)){
+  temp <- rep(grass$G.Incisive.strength[i], round(grass$Herbivory[i]))
+  Incisive.strength <- c(Incisive.strength,temp)
+}
+
+Carbon.nitrogen <- c()
+for(i in 1:nrow(grass)){
+  temp <- rep(grass$P.Leaf.carbon.nitrogen.ratio[i], round(grass$Herbivory[i]))
+  Carbon.nitrogen <- c(Carbon.nitrogen,temp)
+}
+
+#and fit
+pars_grass_freq <- fit_it(integrated_model, 
+                          Tlevel1 = Incisive.strength, 
+                          Tlevel2 = Carbon.nitrogen,
+                          mean_Tlevel1 = mean(Incisive.strength),
+                          sd_Tlevel1 = sd(Incisive.strength))
+                          
+plot.pred(pars = pars_grass_freq, Tlevel1 = Incisive.strength, 
+          Tlevel2 = Carbon.nitrogen, xlab = "C:N ratio", ylab = "Incisive strength")
+#Not sure what is wrong!
+
+#Host/para-------
+host = read.table("data/Tylianakis2008.txt", h = TRUE)
+head(host)
+
+plot(host$parasite_body_length ~ host$host_body_length)
+
+pars_host_bin <- fit_it(integrated_model,
+                        Tlevel1 = host$parasite_body_length, 
+                        Tlevel2 = host$host_body_length,
+                        mean_Tlevel1 = mean(host$parasite_body_length),
+                        sd_Tlevel1 = sd(host$parasite_body_length))
+
+plot.pred(pars = pars_host_bin, Tlevel1 = host$parasite_body_length, 
+          Tlevel2 = host$host_body_length)
+
+#compare models? worth it? the results indicate no trait matching at all.
+
+#pollintors
+pols = read.table("data/Bartomeus2008.txt", h = TRUE)
+head(pols)
+pols <- subset(pols, nectar_holder_depth_mm < 15)
+#Dominique, note that this outyier has a tremendous influence in the model.
+
+pars_pols <- fit_it(integrated_model,
+                    Tlevel1 = pols$IT_mm, 
+                    Tlevel2 = pols$nectar_holder_depth_mm,
+                    mean_Tlevel1 = mean(pols$IT_mm),
+                    sd_Tlevel1 = sd(pols$IT_mm))
+
+plot.pred(pars = pars_pols, Tlevel1 = pols$IT_mm, 
+          Tlevel2 = pols$nectar_holder_depth_mm, xlab = "Nectar depth", ylab = "Pollinator size")
+
+#model comparision
+pars_pol_niche = fit_it(niche_model, 
+                   Tlevel1 = pols$IT_mm, 
+                   Tlevel2 = pols$nectar_holder_depth_mm,
+                   mean_Tlevel1 = mean(pols$IT_mm),
+                   sd_Tlevel1 = sd(pols$IT_mm))
+plot.pred(pars = pars_pol_niche, Tlevel1 = pols$IT_mm, 
+          Tlevel2 = pols$nectar_holder_depth_mm, xlab = "Nectar depth", ylab = "Pollinator size")
+#likelihoods
+lh_model <- -integrated_model(pars_pols, pols$IT_mm, pols$nectar_holder_depth_mm, 
+                              mean(pols$IT_mm), sd(pols$IT_mm))
+lh_niche <- -niche_model(pars_pol_niche, pols$IT_mm, pols$nectar_holder_depth_mm, 
+                         mean(pols$IT_mm), sd(pols$IT_mm))
+lh_neutral <- -neutral_model(pols$IT_mm, pols$nectar_holder_depth_mm, 
+                             mean(pols$IT_mm), sd(pols$IT_mm))
+
+barplot(c(lh_model, lh_niche, lh_neutral))
+
+
