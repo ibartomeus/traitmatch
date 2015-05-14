4067ca018bc0a398ef1e39bcf056173a436af952
ibartomeus <ibartomeus@home>
2015-05-14 13:25:23 +0200

    Modify the max and min allowed par values to relax constrains.

--- R/FitModel_SA.R
+++ R/FitModel_SA.R
@@ -13,8 +13,9 @@ fit_it <- function(model, Tlevel1, Tlevel2, mean_Tlevel1, sd_Tlevel1){
   #pars = c(a0 = lm_M$coefficients[1],a1 = lm_M$coefficients[2],b0 = sd(lm_M$residuals),b1 = 0)
   
   # Boundaries 
-  par_lo = c(a0 = -10, a1 = 0, b0 = -10, b1 = -10) #try different constrains, Here a1 is set to be positive.
-  par_hi = c(a0 = 10, a1 = 10, b0 = 10, b1 = 10)
+  #par_lo = c(a0 = -10, a1 = 0, b0 = -10, b1 = -10) #try different constrains, Here a1 is set to be positive.
+  par_lo = c(a0 = -1000, a1 = -1000, b0 = -100, b1 = -1000) #try different constrains, Here a1 is set to be positive.
+  par_hi = c(a0 = 100, a1 = 5000, b0 = 100, b1 = 1000)
   #Nacho comment: are those especific to the data ranges?
   
   # Maximum likelihood estimation
@@ -44,7 +45,7 @@ plot.pred <- function(pars,Tlevel1, Tlevel2, xlab = "Trait level 2", ylab = "Tra
   
   image(seqX,seqY,Z,xlab = xlab ,ylab = ylab,
         col=heat.colors(10000),cex.axis = 1.25, cex.lab = 1.5, las = 1)
-  points(Tlevel2,Tlevel1,pch = 19, cex = 0.1)  
+  points(Tlevel2,Tlevel1,pch = 19, cex = 0.5)  
 }
 
 
